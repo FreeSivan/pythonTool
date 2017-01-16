a6305b3f2150977ed5c01e673248e45ba13421b7
xiwen.yxw <xiwen.yxw@alibaba-inc.com>
2017-01-16 15:00:51 +0800

    src

--- src/afpRequest.py
+++ src/afpRequest.py
@@ -2,20 +2,29 @@ __author__ = 'xiwen.yxw'
 
 import sys
 import os
+import urllib
+import urllib2
 
 MAX_AFP_SIZE = 768
 AFP_SUFFIX = ".afp"
+HTTP_URL = "http://finger.xiami.com/s/single_search?len="
 
-def offset_loop(fileSize):
+def offsetLoop(fileSize):
     yield ((fileSize/4)/8)*4
     yield (4*((fileSize/4)/8))*4
     yield (7*((fileSize/4)/8))*4
 
 def singleRequest(afp, MAX_AFP_SIZE):
+    realUrl = HTTP_URL + MAX_AFP_SIZE
+    request = urllib2.Request(realUrl)
+    print request
+    request.add_data(afp)
+    response = urllib2.urlopen(request)
+    print response
     return -1
 
 def bufferLoop(afpFile, len):
-    for offset in offset_loop(len):
+    for offset in offsetLoop(len):
         afpFile.seek(offset, 0)
         afpBuf = afpFile.read(MAX_AFP_SIZE)
         yield afpBuf
@@ -23,7 +32,6 @@ def bufferLoop(afpFile, len):
 def afpIdLoop(afpName):
     size = os.path.getsize(afpName)
     fp = open(afpName, 'rb')
-
     for afpBuf in bufferLoop(fp, size):
         yield singleRequest(afpBuf, size)
 
@@ -39,9 +47,8 @@ def afpFileLoop(afpHome):
 def mainLoop(afpHome, outName):
     outFile = open(outName, "w+")
     for f in afpFileLoop(afpHome):
-        line = "" + f + ":"
+        line = f + ":"
         for id in afpIdLoop(f):
-            print "id = %d", id
             line += str(id)
             line += "|"
         line += '\n'
